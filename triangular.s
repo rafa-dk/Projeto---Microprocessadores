f5cd696267750018ffa92a163e2add8b688285da
Rafael Daiki <rafael.daiki@unesp.br>
2025-11-26 08:37:30 -0300

    Logica calculo numero triangular aplicado
    Secao NUM_TRIANGULAR adicionado em triangular.s

--- triangular.s
+++ triangular.s
@@ -4,35 +4,43 @@
 
 ARQTRI:
     # Prologo
-    subi sp, sp, 40
-    stw ra, 36(sp)
-    stw fp, 32(sp)
-    stw r16, 28(sp)
-    stw r17, 24(sp)
-    stw r18, 20(sp)
-    stw r19, 16(sp)
-    stw r20, 12(sp)
+    subi sp, sp, 60
+    stw ra, 56(sp)
+    stw fp, 52(sp)
+    stw r16, 48(sp)
+    stw r17, 44(sp)
+    stw r18, 40(sp)
+    stw r19, 36(sp)
+    stw r20, 32(sp)
     
-    addi fp, sp, 40
+    addi fp, sp, 60
 
     # Inicializacao
     movi r16, 0         # Contador de digitos
     movi r19, 10        # Divisor (10)
     movi r20, 4         # Tamanho da palavra (4 bytes)
+    mov r17, r0         # Variavel para calcular o numero triangular
 
 CONVERTER:
     movia r10, ALAVANCAS
     ldwio r18, (r10)    # Le valor das chaves para r18 (Dividend)
     andi r18, r18, 0xFF # Mascara para 8 bits
+    mov r6, r18
+
+NUM_TRIANGULAR:
+    #Calcula o numero triangular
+    add r18, r18, r17
+    addi r17, r17, 1
+    bne r6, r17, NUM_TRIANGULAR 
 
 DIVISAO:
     # Divide r18 por 10
     # Usamos r18 (callee-saved) para garantir que a chamada de div (se for macro/funcao)
     # nao corrompa o valor do dividendo.
-    call DIV    # r6 = r18 / 10 (quociente)
+    div r6, r18, r19   # r6 = r18 / 10 (quociente)
 
     # Calcula o resto: resto = numero - (quociente * 10)
-    call MULT     # r7 = quociente * 10
+    mul r7, r6, r19   # r7 = quociente * 10
     sub r7, r18, r7     # r7 = r18 - r7 (resto)
 
     # Armazena o digito no buffer
@@ -50,13 +58,13 @@ DIVISAO:
 
 FIM_TRI:
     # Epilogo
-    ldw ra, 36(sp)
-    ldw fp, 32(sp)
-    ldw r16, 28(sp)
-    ldw r17, 24(sp)
-    ldw r18, 20(sp)
-    ldw r19, 16(sp)
-    ldw r20, 12(sp)
-    addi sp, sp, 40
+    ldw ra, 56(sp)
+    ldw fp, 52(sp)
+    ldw r16, 48(sp)
+    ldw r17, 44(sp)
+    ldw r18, 40(sp)
+    ldw r19, 36(sp)
+    ldw r20, 32(sp)
+    addi sp, sp, 60
 
     ret
