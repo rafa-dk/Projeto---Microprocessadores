89b9536a7a186120aa31cb08c9ec71cf0aec835e
Rafael Daiki <rafael.daiki@unesp.br>
2025-11-26 19:24:36 -0300

    Update main.s

--- main.s
+++ main.s
@@ -139,6 +139,17 @@ TRIANGULAR:
 	call UART
 	addi r4, r4,-0x30		#converte para decimal
 	bne r4, r0, POLLING
+	
+WSPACE:
+	ldwio r12, CONTROL(r10)		#leitura de control
+	mov r11, r12		
+	andhi r11, r11, 0xffff		#mascara para wspace
+	beq r11, r0, WSPACE		#caso !wspace retorna
+	movi r4, 32
+	ldw r4, (r4)			#carrega caracter inicio
+	stwio r4, DATA(r10)		#escreve dado em terminal do altera
+	bne r5, r7, WSPACE
+
 	call ARQTRI
 	br POLLING
 
